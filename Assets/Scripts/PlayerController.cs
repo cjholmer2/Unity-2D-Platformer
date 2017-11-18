02173899863ec11aa6bcdcd342433ef4b8bb14c5
Chris Holmer <31230440+cjholmer2@users.noreply.github.com>
2017-11-17 17:06:52 -0800

    Player commits
    added player and movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	//const float jumpHeightMax
+	public float speed;
+	public float jumpHeight = 5;
+	Rigidbody2D rigidBody;
+	bool grounded = true;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Move ();
+
+		if (grounded == true) {
+			Jump ();
+		}
+	}
+
+	void Move() {
+		if(Input.GetKey(KeyCode.D)) {
+			rigidBody.AddForce(transform.right * speed);
+		}
+
+		if(Input.GetKey(KeyCode.A)) {
+			rigidBody.AddForce(transform.right * -speed);
+		}
+	}
+
+
+	void Jump(){
+		if (Input.GetButtonDown ("Jump")) {
+			rigidBody.AddForce (transform.up * jumpHeight);
+			grounded = false;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "Ground") {
+			grounded = true;
+		}
+	}
+
+
+
+}
